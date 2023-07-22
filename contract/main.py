70805237d8040f1122190ff0ad4d6aad5d07a7f8
Leo <leosungun@gmail.com>
2023-07-23 01:02:27 +0300

    app update

---
+++ contract/main.py
@@ -0,0 +1,4 @@
+import requests
+
+print(requests.get('https://2b71-46-135-31-119.ngrok-free.app/get-scores').text
+    )
--- frontend/App.jsx
+++ frontend/App.jsx
@@ -4,24 +4,37 @@ State.init({
   page: "default",
 });
 
+const props = {
+  updatePage: () => {
+    State.update({
+      page: "mint",
+    });
+  },
+};
+
 if (!sender) State.update({ page: "default" });
 if (sender && state.page === "default") State.update({ page: "score" });
 
 if (state.page === "default") {
   return (
     <div>
-      {state.page}
       <Widget src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/MainSPPage" />
     </div>
   );
 }
 
 if (state.page === "score") {
-  return <Widget src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/ScorerPage" />;
-}
-
-if (state.page === "score") {
-  return <Widget src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/ScorerPage" />;
+  return (
+    <div
+      onClick={() => {
+        State.update({
+          page: "mint",
+        });
+      }}
+    >
+      <Widget src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/ScorerPage" />
+    </div>
+  );
 }
 
 return state.page;
