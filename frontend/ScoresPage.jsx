874f0e79732e7c2d7181f4b354168cc26daae35f
Leo <leosungun@gmail.com>
2023-07-22 14:36:49 +0300

    Scores Page update

--- frontend/ScoresPage.jsx
+++ frontend/ScoresPage.jsx
@@ -8,6 +8,13 @@ State.init({
       domains: -1,
       poaps: -1,
     },
+    scores: {
+      gitcoin: -1,
+      nomisPolygon: -1,
+      nomisEth: -1,
+      degenScore: -1,
+      credScore: -1,
+    },
   });
   
   const Wrapper = styled.div`
@@ -58,7 +65,7 @@ State.init({
     }
   `;
   
-  const WalletInfo = styled.div`
+  const ScoreInfo = styled.div`
       display: flex;
       justify-content: center;
       flex-wrap: wrap;
@@ -68,8 +75,29 @@ State.init({
       border: 3px white solid;
       border-radius: 20px;
   
+      hr {
+        width: 100%;
+        border: 2px solid white;
+        background-color: white;
+        color: white;
+      }
   
-      div {
+      .scoreContainer {
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+      }
+  
+      .spScores span {
+        color: #BDFF00;
+      }
+  
+      .spScores {
+        color: white;
+      }
+  
+  
+      .scoreContainer div {
           width: 25%;
           // padding: 0 2.5%;
           color: white;
@@ -80,9 +108,9 @@ State.init({
       display: flex;
       justify-content: center;
       width: 100%;
-  
+      flex-wrap: wrap;
       
-      .box div span {
+      .box .scoreContainer div span {
           color: #BDFF00;
       }
   
@@ -95,6 +123,11 @@ State.init({
   const ScoresContainer = styled.div`
       display: flex;
       align-items: center;
+  
+      button {
+        background-color: #BDFF00!important;
+        color: black!important;
+      }
   `;
   
   return (
@@ -105,31 +138,61 @@ State.init({
       <h1>Scores Passport: your web2 and web3 activity</h1>
       <Scores>
         <ScoresContainer>
-          <div className="title">wallets</div>
-          <WalletInfo className="box">
-            <div>
-              NFTs <span>{state.wallet.nfts}</span>
+          <div className="title">scores</div>
+          <ScoreInfo className="box">
+            <h2 className="spScores">
+              SP Scores <span>10 / 100</span>
+            </h2>
+            <hr />
+            <div className="scoreContainer">
+              <div>
+                Gitcoin <span>{state.wallet.nfts}</span>
+              </div>
+              <div>
+                Nomis Polygon <span>{state.wallet.tokens}</span>
+              </div>
+              <div>
+                Degen <span>{state.wallet.walletAge}</span>
+              </div>
+              <div>
+                Cred <span>{state.wallet.guilds}</span>
+              </div>
+              <div>
+                Nomis Ethereum <span>{state.wallet.transactions}</span>
+              </div>
             </div>
-            <div>
-              Tokens <span>{state.wallet.tokens}</span>
-            </div>
-            <div>
-              Wallet age <span>{state.wallet.walletAge}</span>
-            </div>
-            <div>
-              Guilds <span>{state.wallet.guilds}</span>
-            </div>
-            <div>
-              Transactions <span>{state.wallet.transactions}</span>
-            </div>
-            <div>
-              POAPs <span>{state.wallet.poaps}</span>
-            </div>
-            <div>
-              Domains <span>{state.wallet.domains}</span>
-            </div>
-          </WalletInfo>
+          </ScoreInfo>
+          <button>Mint Score</button>
         </ScoresContainer>
+        <ScoresContainer>
+          <div className="title">wallets</div>
+          <ScoreInfo className="box">
+            <div className="scoreContainer">
+              <div>
+                NFTs <span>{state.wallet.nfts}</span>
+              </div>
+              <div>
+                Tokens <span>{state.wallet.tokens}</span>
+              </div>
+              <div>
+                Wallet age <span>{state.wallet.walletAge}</span>
+              </div>
+              <div>
+                Guilds <span>{state.wallet.guilds}</span>
+              </div>
+              <div>
+                Transactions <span>{state.wallet.transactions}</span>
+              </div>
+              <div>
+                POAPs <span>{state.wallet.poaps}</span>
+              </div>
+              <div>
+                Domains <span>{state.wallet.domains}</span>
+              </div>
+            </div>
+          </ScoreInfo>
+        </ScoresContainer>
+        <ScoresContainer></ScoresContainer>
       </Scores>
     </Wrapper>
   );
