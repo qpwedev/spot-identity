48e49c4c0d851bfbf9c07ca8aa49e1254fe0ea89
Leo <leosungun@gmail.com>
2023-07-23 02:11:21 +0300

    fetch && mintScore update

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -37,4 +37,10 @@ if (state.page === "score") {
   );
 }
 
+if (state.page === "mint") {
+  return (
+    <Widget src="9f8d4bf85f6c2169fccce1deb44c95a010f6b9e682f9887d8b56546c0d5312fe/widget/MintScore" />
+  );
+}
+
 return state.page;
